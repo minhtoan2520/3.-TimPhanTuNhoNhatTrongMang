4e23b1ebdbf5af47f6b02e845d0355bc5757ad37
minhtoan <minhtoan2520@gmail.com>
2026-07-06 09:49:07 +0700

    up

---
+++ MinNumberToArray.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+//BT tim phan tu nho nhat trong mang 1 chieu
+public class MinNumberToArray {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Nhap so phan tu cua mang: ");
+        int size = input.nextInt();
+
+        int num;
+        int[] array = new int[size];
+        for (int i = 0; i < size; i++) {
+            System.out.print("Nhap phan tu thu " + (i + 1) + ": ");
+            num = input.nextInt();
+            array[i] = num;
+        }
+
+        System.out.print("Mang ban vua tao la : [ ");
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i]);
+            if (i < array.length - 1) {
+                System.out.print(", ");
+            }
+        }
+        System.out.print(" ]");
+
+        int min = array[0];
+        int minIndex = 0;
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] < min) {
+                min = array[i];
+                minIndex = i;
+            }
+        }
+        System.out.println("\nPhan tu nho nhat trong mang la: " + min);
+        System.out.println("Nam o vi tri: " + minIndex);
+        input.close();
+    }
+}
